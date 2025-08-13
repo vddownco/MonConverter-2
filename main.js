e1f9a93914acf86c6fda6e3aff75782ac950c8d8
vddownco <info@streamvdos.xyz>
2025-08-12 23:53:57 -0800

    Added index.html

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>Mon Converter</h1>
+    <p class="read-the-docs">
+      Currency conversion application
+    </p>
+  </div>
+`
